5ddbdb91d08a04a03010c6a39a0154c7cf91205c
abhishek <alabhishek17@gmail.com>
2024-03-18 23:44:37 +0530

    imgsearch

---
+++ script.js
@@ -0,0 +1,54 @@
+let searchBtn=document.getElementById("searcBtn");
+let searchVal=document.getElementById("searchVal");
+let mainImg=document.getElementById("main-img");
+
+// async function imgeDisplay(){
+
+// }
+searchBtn.addEventListener("click",imgeDisplay);
+
+// function imgeDisplay(){
+    // const accessKey = 'lo0FTTa2hv_QUrmGp2QSgNNSI-Y-Z0dOJyaMsoZwJM4';
+//     async function ajay(){
+//         let url=
+//             let res=await fetch(" https://api.unsplash.com/ ",{
+//                 method:'GET',
+//                 headers: {'X-Api-Key': 'sZtWySsoMxqHMq0IwOyrf_f5ZH8qc-60eXn2q-C1N6A'},
+//                 ContentType:"application/json"
+        
+//             });
+//             let data=await  res.json();
+//             console.log(data);
+//         }
+// // console.log(result);
+// ajay();
+// }
+// imgeDisplay();
+
+
+const accessKey = 'lo0FTTa2hv_QUrmGp2QSgNNSI-Y-Z0dOJyaMsoZwJM4';
+function imgeDisplay(){
+async function searchImages() {
+    inputData = searchVal.value;
+    const url = `https://api.unsplash.com/search/photos?query=${inputData}&client_id=${accessKey}`;
+  
+    const response = await fetch(url);
+    const data1 = await response.json();
+    // console.log(data);
+
+    // let addImg="";
+    // data1.data.forEach(element => {
+    //     console.log(element);
+    //     // addImg+=`<div>
+    //     // <img src"${element.url.small} alt"animal"> 
+    //     // </div>`
+    // });
+    // mainImg.innerHTML=addImg;
+
+    let result=data1.result;
+    console.log(result);
+    
+}
+
+searchImages()
+}
